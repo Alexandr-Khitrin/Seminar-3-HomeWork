afc8aeeeb4f0cab5b1a827d7f7ac2e2cbcc4ea85
Alexandr <hitrin79@gmail.com>
2022-12-07 17:09:50 +0800

    Zadacha 19

---
+++ HomeWork Seminar-3 Zadacha 19/HomeWork Seminar-3 Zadacha 19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string palindrome = number.ToString();
+
+if(palindrome[0] == palindrome[4] && palindrome[1] == palindrome[3])
+{
+    Console.WriteLine($"Число {number} является полиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является полиндромом");
+}
