c1a29d9a105cce005cc2a85db850dd79360bbd04
Alexandr <hitrin79@gmail.com>
2022-12-07 16:48:02 +0800

    Zadacha 21

---
+++ HomeWork Seminar-3 Zadacha 21/HomeWork Seminar-3 Zadacha 21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21
+Console.WriteLine("Введите первую точку: ");
+Console.Write("Введите значение X: ");
+double ax = double.Parse(Console.ReadLine());
+Console.Write("Введите значение Y: ");
+double ay = double.Parse(Console.ReadLine());
+Console.Write("Введите значение Z: ");
+double az = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую точку: ");
+Console.Write("Введите значение X: ");
+double bx = double.Parse(Console.ReadLine());
+Console.Write("Введите значение Y: ");
+double by = double.Parse(Console.ReadLine());
+Console.Write("Введите значение Z: ");
+double bz = double.Parse(Console.ReadLine());
+
+double ab = Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2); // Возведение в степень
+
+ab = Math.Sqrt(ab); // Вычисление квадратного корня
+ab = Math.Round(ab, 2);
+
+Console.WriteLine(ab);
